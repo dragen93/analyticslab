bb6700f0bd70f37a2642cfebc3851b3b74abb18c
dragen <dragenhunter200@gmail.com>
2022-09-11 23:50:54 +0500

    apps links added

--- analyticsLab/urls.py
+++ analyticsLab/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('analyticsLab',views.analyticsLab, name="analyticslab"),
+    path('dashboard',views.analyticsLab, name="analyticslab"),
     path('searchDatabase',views.searchDatabase, name="searchDatabase"),
 ]
 
--- analyticsLab/views.py
+++ analyticsLab/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def analyticsLab(request):
-    return render(request, "analyticsLab/index.html")
+    return render(request, "analyticsLab/dashboard.html")
 
 def searchDatabase(request):
     return render(request, "analyticsLab/searchDatabase.html")
